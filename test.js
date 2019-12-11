8206e6ca45a73a7de97d80cbc1434405730e26e7
Glitch (shell-sandalwood) <none>
2019-12-11 05:28:46 +0000

    😜😹 Checkpoint ./test.js:6601014/169 ./index.js:6601014/29855

--- test.js
+++ test.js
@@ -4,15 +4,15 @@ const Music = require('./index.js');
 const music = new Music(bot, {
 	lavalink: {
 		"restnode": {
-			"host": "localhost",
+			"host": "http://144.172.70.233:8080",
 			"port": 3000,
-			"password":"youshallnotpass"
+			"password":"projectXbot85645"
 		},
 		"nodes": [
-			{ "host": "localhost", "port": 3000, "region": "asia", "password": "youshallnotpass" }
+			{ "host": "http://144.172.70.233:8080", "port": 3000, "region": "asia", "password": "projectXbot85645" }
 		]
 	},
-	admins: ["455346525716086795"]
+	admins: ["518731387591720961"]
 });
 
-bot.login("NjUzOTkzMTg2ODQ2NTcyNTQ1.Xe_EzA.3SX4xf50ndeFNK_ETHBxpmUSvv8");
+bot.login("NjUzOTkzMTg2ODQ2NTcyNTQ1.Xe_EzA.3SX4xf50ndeFNK_ETHBxpmUSvv8");I
