92d3b8a9414390c1b1fdb4db4b5b0b496a95f8be
Glitch (shell-sandalwood) <none>
2019-12-12 16:24:53 +0000

    🐮🌉 Checkpoint ./index.js:2717930/264

--- index.js
+++ index.js
@@ -14,7 +14,6 @@
 */
 
 const http = require('http');
-http.createServer().listen(process.env.PORT);
 const Discord = require('discord.js');
 const PACKAGE = require('./package.json');
 const Lavalink = require('discord.js-lavalink');
